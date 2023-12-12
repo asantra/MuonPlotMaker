071b65b27e1d875f30494daf6a969affcec3524e
asantra <santra.arka@gmail.com>
2023-12-12 19:50:18 +0530

    log axis for sigma

--- inputFunctions.h
+++ inputFunctions.h
@@ -81,6 +81,25 @@ int getMuonSize(ROOT::VecOps::RVec<float> muon_pt)
     return nMuon;
 }
 
+/*
+///  using log binning on a histogram
+void BinLogX(TH1 *h)
+{
+   TAxis *axis = h->GetXaxis();
+   int bins = axis->GetNbins();
+
+   Axis_t from = axis->GetXmin();
+   Axis_t to = axis->GetXmax();
+   Axis_t width = (to - from) / bins;
+   Axis_t *new_bins = new Axis_t[bins + 1];
+
+   for (int i = 0; i <= bins; i++) {
+     new_bins[i] = TMath::Power(10, from + i * width);
+   }
+   axis->Set(bins, new_bins);
+   delete new_bins;
+}
+*/
 
 /// prepare many 2D maps with one key
 class manyMaps{
